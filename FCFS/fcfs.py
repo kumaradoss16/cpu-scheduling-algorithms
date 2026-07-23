9ddd46bece5d1873e9c240541bda7d4ed6287f6a
Kumaradoss <76904568+kumaradoss16@users.noreply.github.com>
2026-07-23 10:19:16 +0530

    Refactor FCFS scheduling output and calculations

--- FCFS/fcfs.py
+++ FCFS/fcfs.py
@@ -5,7 +5,7 @@ burst_time = []
 
 #Input process details
 for i in range(n):
-    process = int(input(f"Enter Process ID (e.g., {i+1}): "))
+    process = int(input(f"Enter Process ID (e.g., P{i+1}): "))
     at = int(input(f"Enter Arrival Time for Process {process}: "))
     bt = int(input(f"Enter Burst Time for Process {process}: "))
     processes.append(process)
@@ -20,15 +20,21 @@ processes, arrival_time, burst_time = zip(*combined)   # Separate the List Again
 completion_time = []
 turnaround_time = []
 waiting_time = []
+start_time = []
+total_idle = 0
 
 current_time = 0
 
-# Calculate Completion Time
+# Calculate Start and Completion Time
 for i in range(n):
+    # CPU idle
     if current_time < arrival_time[i]:
+        total_idle += arrival_time[i] - current_time
         current_time = arrival_time[i]
-
+        
+    # Execute Process
     current_time += burst_time[i]
+    #Store Completion Time
     completion_time.append(current_time)
 
 # Calculate TAT, WT and its Average
@@ -40,6 +46,7 @@ for i in range(n):
 
 avg_tat = sum(turnaround_time) / n
 avg_wt = sum(waiting_time) / n
+schedule_length = max(completion_time) - min(arrival_time)
 
 
 print("\nFSFC Scheduling Result")
@@ -48,9 +55,11 @@ print(f"{'Process':<10}{'AT':<8}{'BT':<8}{'CT':<8}{'TAT':<8}{'WT':<8}")
 print("-" * 72)
 
 for i in range(n):
-    print(f"{processes[i]:<10}{arrival_time[i]:<8}{burst_time[i]:<8}{completion_time[i]:<8}{turnaround_time[i]:<8}{waiting_time[i]:<5
-    8}")
+    print(f"{processes[i]:<10}{arrival_time[i]:<8}{burst_time[i]:<8}{completion_time[i]:<8}{turnaround_time[i]:<8}{waiting_time[i]:<8}")
 
 print("-" * 72)
 print(f"Average Turnaround Time: {avg_tat:.2f}")
 print(f"Average Waiting Time: {avg_wt:.2f}")
+print(f"Total CPU Idle Time: {total_idle:.2f}")
+print(f"Schedule Length = {schedule_length:.2f}")
+
